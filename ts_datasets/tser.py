db271ef8c1ccd41c91d6895ec8701893b92f073a
Stephen Fox <stevemasta34@gmail.com>
2023-07-12 23:59:11 -0400

    Cleaned up the documentation

--- ts_datasets/tser.py
+++ ts_datasets/tser.py
@@ -1,3 +1,26 @@
+"""
+The TSER Monash-UEA-UCR archive gives us we have N-many sequences of M in length e.g. (N, M, n_features).
+The number of columns or features (``n_features``) vary in the supplied ``df``, based on the dataset.
+- With one exceptiont, all datasets have the same number of time steps per feature.
+
+NOTE: If you're doing deep learning, this would be a great dataset for experimentation with the embedding of a sequence.
+
+The Time Series Extrinsic Regression problem is to predict a single value from the entire sequence,
+rather than having single for each time step (such as shown in e.g. Google's Machine Learning Crash Course).
+
+The *only* label (and therefore, signal) is for the whole sequence
+- It may or may not be from the final time step of the sequence e.g. total kwH consumed by a household's appliance usage
+- It may be a separate value e.g. approximate BPM given 12s of 3 different vital signs
+
+so one might be able to derive values for each time step - but that is not the explicit goal.
+
+We believe this better represents the real world, where we don't have labels at every time step for e.g. time-to-event prediction
+but will experience some event and work backwards to find some descriptive labels with e.g. subject-matter experts.
+
+This contrasts with e.g. industry giant Amazon's perspective in their Gluon Time Series library, where they are
+forecasting outputs per time step within the same domain rather than mapping to a new domain for their predictions.
+"""
+
 from pathlib import Path
 
 from pandas import Series, concat, DataFrame
@@ -5,22 +28,11 @@ import torch as pt
 
 from .tsextrinsic_regression_data_loader import load_from_tsfile_to_dataframe, regression_datasets as REGRESSION_DS_NAMES
 
-
 def dataset_rows_to_train_val_tensors(df: DataFrame,
                                       targets_array: Series,
                                       *,
                                       validation_portion=0.2):
     """Convert the dataframe and targets from the TSER load function into PyTorch tensors.
-    
-    The TSER Monash-UEA-UCR archive gives us we have N-many sequences of M in length e.g. (N, M, *).
-    The number of features (``n_features``) vary in the supplied ``df``.
-    NOTE: this would be a great time to experiment with the embedding of a sequence.
-    
-    The problem is to predict a single value from the entire sequence, rather than having single for each time step.
-    The *only* signal is from the final time step, so one might be able to derive values for each time step - but that is
-    not the explicit goal.
-    We beliieve this better represents the real world, where we don't have labels at every time step for e.g. time-to-event prediction
-    but will experience some event and work backwards to find some descriptive labels with e.g. subject-matter experts.
     
     Args:
         df:
@@ -38,8 +50,8 @@ def dataset_rows_to_train_val_tensors(df: DataFrame,
     tens = [pt.tensor(d.values) for d in dfs]
     tens = pt.stack(tens)
     tens_targets = pt.tensor(targets_array)
-    out = ((tens[:-val_len], tens_targets[:-val_len]), (tens[-val_len:], tens_targets[-val_len:]))
-    return out
+    train_val_tensors = ((tens[:-val_len], tens_targets[:-val_len]), (tens[-val_len:], tens_targets[-val_len:]))
+    return train_val_tensors
 
 
 def process_regression_dataset(dataset_archive_root: Path,
@@ -71,46 +83,31 @@ def process_regression_dataset(dataset_archive_root: Path,
         raise ValueError(
             "'PPGDalia' dataset is current unsupported; its data alignment is non-standard")
 
-    DEBUG = True
-
     trainables_output_dir = dataset_archive_root / "trainable-tensors"
 
-    # FIXME(stephen): it should probably be segregated by file format.
     mvts_data_root = unzip_root / "Monash_UEA_UCR_Regression_Archive"
 
     mvts_data_root.mkdir(parents=True, exist_ok=True)
     trainables_output_dir.mkdir(parents=True, exist_ok=True)
 
-    if DEBUG:
-        print("Files and Folders in the data root directory:\n", list(mvts_data_root.iterdir()))
-
     dataset_dir = mvts_data_root / dataset_dir_name
 
     # mvts: Multivariate Time Series sequence
-    mvts_train_df, mvts_train_targets = load_from_tsfile_to_dataframe(
-        dataset_dir / f"{dataset_dir_name}_TRAIN.ts")
+    mvts_train_df, mvts_train_targets = load_from_tsfile_to_dataframe(dataset_dir /
+                                                                      f"{dataset_dir_name}_TRAIN.ts")
     mvts_test_df, mvts_test_targets = load_from_tsfile_to_dataframe(dataset_dir /
                                                                     f"{dataset_dir_name}_TEST.ts")
 
     processed_train_val = dataset_rows_to_train_val_tensors(mvts_train_df, mvts_train_targets)
-    if DEBUG:
-        print("df_{train,test}.shape:", mvts_train_df.shape, mvts_test_df.shape)
-        if mvts_train_df.shape[-1] > 1:
-            print("df.iloc[0, [0, 1]]:", mvts_train_df.iloc[0, 0], mvts_train_df.iloc[0, 1])
-        else:
-            print("df.iloc[0, 0]:", mvts_train_df.iloc[0, 0])
-        print("df.iloc[0, -1]:", mvts_train_df.iloc[0, -1])
-        print("Output tensor sizes (train, val):\n", [
-            (feat_tens.size(), target_tens.size()) for feat_tens, target_tens in processed_train_val
-        ])
 
     # train
     pt.save(processed_train_val, f"{trainables_output_dir}/{dataset_dir_name}_TRAIN.pt")
 
     # test
-    stacked_tensors_and_targets = (pt.stack([
+    stacked_tensors = pt.stack([
         pt.tensor(concat(row_tup, axis='columns').values)
         for _, *row_tup in mvts_test_df.itertuples()
-    ]), pt.tensor(mvts_test_targets))
+    ])
+    targets = pt.tensor(mvts_test_targets)
 
-    pt.save(stacked_tensors_and_targets, f"{trainables_output_dir}/{dataset_dir_name}_TEST.pt")
+    pt.save((stacked_tensors, targets), f"{trainables_output_dir}/{dataset_dir_name}_TEST.pt")
